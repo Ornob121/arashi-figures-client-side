069f6efd9aa1d22ef8b99cfa72f323b6836c285d
Ornob121 <shadmantahmid121@gmail.com>
2023-05-20 17:06:30 +0600

    responsive fixed

--- src/Pages/Home/ProductPic/ProductPic.jsx
+++ src/Pages/Home/ProductPic/ProductPic.jsx
@@ -6,42 +6,42 @@ const ProductPic = () => {
         Here Are Some Of Our Product Images
       </h3>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-10">
-        <div data-aos="slide-down" data-aos-delay="200">
+        <div data-aos="slide-down" data-aos-delay="200" className=" mx-auto">
           <img
             className="w-[500px] h-[400px] shadow-md rounded-md"
             src="https://i.ibb.co/dL31dYb/img1.jpg"
             alt=""
           />
         </div>
-        <div data-aos="slide-down" data-aos-delay="200">
+        <div data-aos="slide-down" data-aos-delay="200" className=" mx-auto">
           <img
             className="w-[500px] h-[400px] shadow-md rounded-md"
             src="https://i.ibb.co/RcdkPWN/img2.png"
             alt=""
           />
         </div>
-        <div data-aos="slide-down" data-aos-delay="200">
+        <div data-aos="slide-down" data-aos-delay="200" className=" mx-auto">
           <img
             className="w-[500px] h-[400px] shadow-md rounded-md"
             src="https://i.ibb.co/BnnXKXs/img3.jpg"
             alt=""
           />
         </div>
-        <div data-aos="slide-up" data-aos-delay="400">
+        <div data-aos="slide-up" data-aos-delay="400" className=" mx-auto">
           <img
             className="w-[500px] h-[400px] shadow-md rounded-md"
             src="https://i.ibb.co/jHFtwRp/img4.jpg"
             alt=""
           />
         </div>
-        <div data-aos="slide-up" data-aos-delay="300">
+        <div data-aos="slide-up" data-aos-delay="300" className=" mx-auto">
           <img
             className="w-[500px] h-[400px] shadow-md rounded-md"
             src="https://i.ibb.co/2h7sRyd/img5.jpg"
             alt=""
           />
         </div>
-        <div data-aos="slide-up" data-aos-delay="400">
+        <div data-aos="slide-up" data-aos-delay="400" className=" mx-auto">
           <img
             className="w-[500px] h-[400px] shadow-md rounded-md"
             src="https://i.ibb.co/KWvMLSy/img6.jpg"
